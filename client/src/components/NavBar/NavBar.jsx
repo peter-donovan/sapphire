595a82746cacc98d8af916702f2eb66778f8adfa
Peter Donovan <pjd720@yahoo.com>
2021-02-23 20:39:06 -0500

    add navbar menu dropdown for smaller screens

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { Gem } from '@styled-icons/fa-regular/Gem';
 
-import { colors, fonts } from 'theme/variables';
+import { breakpoints, colors, fonts } from 'theme/variables';
 
 const NavBar = () => {
+	const [open, setOpen] = useState(false);
+
 	return (
 		<Nav>
 			<Logo to="/">
-				<Gem size="22.5" title="application logo" />
+				<Gem size="25" title="application logo" />
 				<h1>Sapphire</h1>
 			</Logo>
-			<HamburgerIcon>
+			<HamburgerIcon open={open} onClick={() => setOpen(!open)}>
 				<span />
 				<span />
 				<span />
 			</HamburgerIcon>
-			<Menu role="navigation">
+			<Menu role="navigation" open={open}>
 				<MenuItem>
 					<NavLink to="/chat">Chat</NavLink>
 				</MenuItem>
@@ -41,7 +43,7 @@ const Nav = styled.nav`
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: space-between;
-	padding: 0 1.25rem;
+	padding: 0 1.5rem;
 `;
 
 const Logo = styled(Link)`
@@ -51,31 +53,88 @@ const Logo = styled(Link)`
 
 	svg {
 		color: ${colors.brand.primary};
-		margin: 0.35rem 0.25rem 0 0;
+		margin: 0.25rem 0.25rem 0 0;
 	}
 
 	h1 {
 		font-weight: ${fonts.weight.black};
 		padding: 0.5rem 0;
-		text-transform: uppercase;
+		text-shadow: 2px 2px 2px ${colors.element.black};
 	}
 `;
 
 const HamburgerIcon = styled.div`
+	cursor: pointer;
+	display: none;
+	flex-direction: column;
+	height: 2rem;
+	width: 2rem;
+	position: fixed;
+	top: 0.65rem;
+	right: 1.75rem;
+
+	@media (max-width: ${breakpoints.md}) {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: space-around;
+	}
+
 	span {
-		color: white;
-		height: 2px;
-		width: 1rem;
+		background-color: ${colors.brand.light};
+		border-radius: 10px;
+		height: 0.25rem;
+		width: 2rem;
+		transform-origin: 1px;
+		transition: all 200ms linear;
+
+		&:nth-child(1) {
+			transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+		}
+
+		&:nth-child(2) {
+			//transform: ${({ open }) => (open ? 'translateX(100%)' : 'translateX(0)')};
+			opacity: ${({ open }) => (open ? 0 : 1)};
+		}
+
+		&:nth-child(3) {
+			transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+		}
 	}
 `;
 
 const Menu = styled.ul`
+	align-items: center;
 	display: flex;
-	flex-direction: row;
+	justify-content: space-between;
 	margin: 0;
-	padding: 0;
+	position: relative;
+
+	@media (max-width: ${breakpoints.md}) {
+		flex-direction: row;
+		justify-content: space-around;
+		height: ${({ open }) => (open ? '3rem' : '0px')};
+		overflow: hidden;
+		transition: height 300ms ease-in;
+		width: 100%;
+	}
 `;
 
 const MenuItem = styled.li`
-	font-size: 0.95em;
+	font-size: 0.9em;
+
+	a {
+		color: ${colors.brand.light};
+		padding: 1rem 2rem;
+		text-align: center;
+		text-decoration: none;
+		text-transform: uppercase;
+		text-shadow: 2px 2px 1px ${colors.element.black};
+		transition: all 300ms ease-in;
+
+		&:active,
+		&:focus,
+		&:hover {
+			color: ${colors.brand.primary};
+		}
+	}
 `;
