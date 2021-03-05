63d042b8aebecce7200f3d56d45cf0178b615198
Peter Donovan <pjd720@yahoo.com>
2021-03-04 19:25:31 -0500

    replace React client 'sapphire-ui' with Vue.js

---
+++ sapphire-client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-app id="app">
+		<NavigationBar />
+
+		<v-main>
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+	</v-app>
+</template>
+
+<style lang="scss">
+.main {
+	text-align: center;
+}
+//#app {
+//	font-family: Avenir, Helvetica, Arial, sans-serif;
+//	-webkit-font-smoothing: antialiased;
+//	-moz-osx-font-smoothing: grayscale;
+//	text-align: center;
+//	color: #2c3e50;
+//}
+
+//#nav {
+//	padding: 30px;
+//
+//	a {
+//		font-weight: bold;
+//		color: #2c3e50;
+//
+//		&.router-link-exact-active {
+//			color: #42b983;
+//		}
+//	}
+//}
+</style>
+<script>
+import NavigationBar from '@/components/NavigationBar';
+export default {
+	components: { NavigationBar },
+};
+</script>
---
+++ sapphire-client/src/components/NavigationBar.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-app-bar class="px-3" app>
+		<v-toolbar-title>
+			<router-link to="/">
+				<h2 class="font-weight-black text-uppercase white--text">
+					<v-icon class="icon" color="blue lighten-3" large>mdi-diamond-outline</v-icon>
+					Sapphire
+				</h2>
+			</router-link>
+		</v-toolbar-title>
+		<v-spacer />
+		<v-app-bar-nav-icon class="mdi-diamond-outline"> </v-app-bar-nav-icon>
+		<v-navigation-drawer app> </v-navigation-drawer>
+		<v-toolbar-items class="align-center flex justify-end" v-for="(link, name) in routes" :key="name">
+			<router-link class="text-uppercase" :to="link">
+				{{ name }}
+			</router-link>
+		</v-toolbar-items>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	name: 'NavigationBar',
+	data() {
+		return {
+			routes: {
+				home: '/',
+				chat: '/chat',
+				register: '/register',
+				login: '/login',
+			},
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.spacer {
+	flex-grow: 2;
+}
+
+a {
+	text-decoration: none;
+}
+</style>
